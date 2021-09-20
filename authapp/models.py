9142bb36cce819182a31ecb830125b2590e5e38e
ilya <ziluha491@gmail.com>
2021-09-20 18:54:05 +0300

    update profile.html

--- authapp/models.py
+++ authapp/models.py
@@ -1,5 +1,7 @@
 from datetime import timedelta
 
+from Tools.demo.mcast import receiver
+from django.db.models.signals import post_save
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.tamezone import now
@@ -47,3 +49,18 @@ class ShopUserProfile(models.Model):
         blank=True,
     )
 
+    gender = models.CharField(
+        verbose_name='пол',
+        max_length=1,
+        choices=GENDER_CHOISES,
+        blank=True
+    )
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
